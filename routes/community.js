b2e731e3beca700883be68db15faf4af77d4e6ed
Zura Kurtanidze <zura.kurta@gmail.com>
2021-01-24 21:43:20 +0400

    💥 Join in community.

--- routes/community.js
+++ routes/community.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { Community, validate } = require("../models/Community");
+const { User } = require("../models/User");
 const auth = require("../middleware/auth");
 
 router.get("/", async (req, res) => {
@@ -21,6 +22,19 @@ router.post("/", auth, async (req, res) => {
 router.get("/:id", async (req, res) => {
   const community = await Community.findById(req.params.id);
   if (!community) res.status(404).send("No community found with given ID");
+
+  res.send(community);
+});
+
+router.post("/:id/join", auth, async (req, res) => {
+  let user = await User.findById(req.user._id);
+  if (!user) res.status(400).send("Bad Request.");
+
+  let community = await Community.findById(req.params.id);
+  if (!community) res.status(404).send("Community not found.");
+
+  community.members.push(user._id);
+  community.save();
 
   res.send(community);
 });
