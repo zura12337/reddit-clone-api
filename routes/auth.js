b21d9273aa7e071b633334d5b0517c78a66b56e3
Zura <zura.kurta@gmail.com>
2021-03-17 18:45:10 +0400

    :sparkles: Add google auth route.

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const { User } = require("../models/User");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 router.post("/", async (req, res) => {
   let user = await User.findOne({ email: req.body.email });
@@ -13,6 +14,23 @@ router.post("/", async (req, res) => {
   const token = user.generateAuthToken();
 
   res.cookie("token", token, { httpOnly: true }).send(token);
+});
+
+router.post("/google", async (req, res) => {
+  const token = req.body.tokenId;
+  const decoded = jwt.decode(token);
+
+  let user = await User.findOne({ email: decoded.email });
+  if (!user) return res.status(400).send("User not registered");
+
+  const currentTime = Math.floor(Date.now() / 1000);
+  if (currentTime < decoded.exp) {
+    let token = user.generateAuthToken();
+
+    res.cookie("token", token, { httpOnly: true }).send(token);
+  } else {
+    res.status(400).send("Cann't Login.");
+  }
 });
 
 module.exports = router;
