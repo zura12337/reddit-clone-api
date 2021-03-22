bb49fd62b5666f485e0b8f098ed2601ecc32144e
Zura <zura.kurta@gmail.com>
2021-03-23 00:53:48 +0400

    🔚 Add trending communities route.

--- routes/community.js
+++ routes/community.js
@@ -4,6 +4,7 @@ const { Community, validate } = require("../models/Community");
 const { User } = require("../models/User");
 const auth = require("../middleware/auth");
 const isAdmin = require("../middleware/isAdmin");
+const querystring = require("querystring");
 
 router.get("/", async (req, res) => {
   const community = await Community.find();
@@ -12,7 +13,9 @@ router.get("/", async (req, res) => {
 });
 
 router.get("/trending", async (req, res) => {
-  const community = await Community.find().sort({ members: 1 });
+  const limit = req.query.limit;
+
+  const community = await Community.find().sort({ members: 1 }).limit(limit);
   res.send(community);
 });
 
