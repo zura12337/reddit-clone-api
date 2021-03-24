5edc67511d8d8e26121feed1de7897039bc04b6c
Zura <zura.kurta@gmail.com>
2021-03-24 15:58:36 +0400

    :sparkles: Create DraftPost Model.

---
+++ models/DraftPost.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const draftPostSchema = new Schema({
+  title: String,
+  body: String,
+  image: String,
+  url: String,
+  postedTo: String,
+  date: String,
+  postedBy: { type: Schema.Types.ObjectId, ref: "User" },
+});
+
+const DraftPost = mongoose.model("DraftPost", draftPostSchema);
+
+exports.DraftPost = DraftPost;
