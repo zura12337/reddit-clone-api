27f0124fcf39ab744906bdaa2d8e9ad90475c060
Zura Kurtanidze <zura.kurta@gmail.com>
2021-06-11 20:26:52 +0400

    :art: change draft post schema.

--- models/DraftPost.js
+++ models/DraftPost.js
@@ -7,7 +7,7 @@ const draftPostSchema = new Schema({
   body: String,
   image: String,
   url: String,
-  postedTo: String,
+  postedTo: { type: { label: String, value: String } },
   date: String,
   postedBy: { type: Schema.Types.ObjectId, ref: "User" },
 });
